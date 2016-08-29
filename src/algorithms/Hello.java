a852539b6b320e48f18e8bf5ded652791a3a90c5
crimsonLin <crimsonlin@gmail.com>
2016-08-29 23:24:17 +0800

    first commit hello.java

---
+++ src/algorithms/Hello.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Across the Great Wall we can reach every corner in the world");
+
+	}
+
+}
