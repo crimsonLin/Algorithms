ad35aee1ad0dfa301f542d88a4aabf32fc005280
crimsonLin <crimsonlin@gmail.com>
2016-09-02 00:50:20 +0800

    commit

--- src/algorithms/Hello.java
+++ src/algorithms/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Across the Great Wall we can reach every corner in the world");
-
+		//The following practice is from Sedgewick's algorithms( 4th edition)
 	}
 
 }
